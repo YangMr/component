f71161126e5f2b5503d1857ae04f52e6f3344554
cstp <759168442@qq.com>
2022-07-18 22:28:20 +0800

    doc(文档):更新README.md文档

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import './styles/icon/iconfont.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
