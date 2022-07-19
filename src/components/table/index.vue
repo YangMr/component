c2c95df04d430e30ed7b9ae2477820f2c19017dc
cstp <759168442@qq.com>
2022-07-19 09:12:30 +0800

    feat(新功能):table组件封装 - 插槽渲染组件

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -5,18 +5,13 @@
       style="width: 100%">
       <el-table-column v-if="index" label="序号" type="index" width="55"></el-table-column>
       <el-table-column v-if="checkbox" type="selection" width="55"></el-table-column>
-      <template v-for="(item) in column">
-        <el-table-column v-if="item.type === 'function'" :key="item.prop" :prop="item.prop" :label="item.label" :width="item.width">
+      <template v-for="(item,index) in column">
+        <el-table-column v-if="item.type === 'function'"  :key="index" :prop="item.prop" :label="item.label" :width="item.width">
           <template v-slot="scope">
-            <div v-html="item.callback && item.callback(scope.row)"></div>
+            <div v-html="item.callback && item.callback(scope.row,index)"></div>
           </template>
         </el-table-column>
-        <el-table-column v-if="item.type === 'slot'" :key="item.prop" :prop="item.prop" :label="item.label" :width="item.width">
-          <template >
-            <slot :name="item.slot_name"></slot>
-          </template>
-        </el-table-column>
-        <el-table-column v-else  :key="item.prop" :prop="item.prop" :label="item.label" :width="item.width"></el-table-column>
+        <el-table-column v-else :key="index" :prop="item.prop" :label="item.label" :width="item.width"></el-table-column>
       </template>
     </el-table>
   </div>
@@ -24,7 +19,7 @@
 
 <script>
 export default {
-  name: 'Table',
+  name: 'table',
   props: {
     column: {
       type: Array,
@@ -36,25 +31,31 @@ export default {
   data () {
     return {
       tableData: [{
+        id: 1,
+        title: '这是一段标题1',
         date: '2016-05-02',
         name: '王小虎',
-        address: '上海市普陀区金<div>234234</div>沙江路 1518 弄',
+        address: '上海市<b>普陀</b><div>123456</div>区金沙江路 1518 弄',
         sex: '男'
       }, {
+        id: 2,
+        title: '这是一段标题2',
         date: '2016-05-04',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1517 弄',
         sex: '女'
       }, {
+        title: '这是一段标题3',
         date: '2016-05-01',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1519 弄',
-        sex: '男'
+        sex: '女'
       }, {
+        title: '这是一段标题4',
         date: '2016-05-03',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1516 弄',
-        sex: '女'
+        sex: '男'
       }]
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home">
-    <yang-table :column="column" checkbox index>
-      <template v-slot:operation>
-        <el-button type="primary">编辑</el-button>
-        <yang-button type="danger">删除</yang-button>
-      </template>
-    </yang-table>
+    <yang-table :column="column" index checkbox></yang-table>
   </div>
 </template>
 
@@ -17,24 +12,27 @@ export default {
     return {
       column: [
         {
-          label: 'URL地址',
+          label: '标题',
+          prop: 'title',
           type: 'function',
-          prop: 'date',
-          callback: (data) => {
-            return `<a href="https://www.baidu.com">${data.name}</a>`
+          callback: (row) => {
+            if (row.id === 1) {
+              return `<a href="https://www.baidu.com">${row.title}</a>`
+            }
+            return `<span>${row.title}</span>`
+            // return `<strong>${row.title}</strong>`
+            // return row.title.replace('标题', '<strong>标题</strong>')
           }
         },
-        // { label: '日期', prop: 'date', width: 500 },
+        { label: '日期', prop: 'date' },
         { label: '姓名', prop: 'name' },
         { label: '地址', prop: 'address' },
-        { label: '性别', prop: 'sex' },
-        { label: '操作', prop: 'operation', type: 'slot', slot_name: 'operation' }
+        { label: '性别', prop: 'sex' }
       ]
     }
   },
   components: {
-    yangTable: () => import('../components/table/index.vue'),
-    yangButton: () => import('../components/button/index.vue')
+    yangTable: () => import('../components/table/index.vue')
   },
   methods: {
 
